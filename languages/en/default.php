407ec78d59dbc718829e6bbc866a4ac43f375ffe
Manuel Rogoll <victorios@me.com>
2014-10-02 13:56:45 +0200

    fix spelling issues

--- languages/en/default.php
+++ languages/en/default.php
@@ -5,8 +5,8 @@
 $GLOBALS['TL_LANG']['CTE']['contaoMaps']                 = 'ContaoMaps';
 $GLOBALS['TL_LANG']['tl_content']['contaoMaps_legend']   = 'Map';
 $GLOBALS['TL_LANG']['CTE']['map_list'][0]                = 'All maps';
-$GLOBALS['TL_LANG']['CTE']['map_list'][1]                = 'get all maps';
+$GLOBALS['TL_LANG']['CTE']['map_list'][1]                = 'Get all maps';
 $GLOBALS['TL_LANG']['CTE']['single_map'][0]              = 'Single map';
-$GLOBALS['TL_LANG']['CTE']['single_map'][1]              = 'get one map';
-$GLOBALS['TL_LANG']['tl_contaoMaps']['ce']['cmMapId'][0] = 'select map';
+$GLOBALS['TL_LANG']['CTE']['single_map'][1]              = 'Get one map';
+$GLOBALS['TL_LANG']['tl_contaoMaps']['ce']['cmMapId'][0] = 'Select map';
 $GLOBALS['TL_LANG']['tl_contaoMaps']['ce']['cmMapId'][1] = 'Select a existing map';
