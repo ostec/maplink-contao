052d9f75e8cef6c0e49e4f62ccf6b048deb9fe71
Manuel Rogoll <victorios@me.com>
2014-10-07 11:47:27 +0200

    fixing cannot install contaoMaps

--- dca/tl_content.php
+++ dca/tl_content.php
@@ -8,41 +8,45 @@
  */
 
 $maps = array( 0 => '' );
-$rs   = DATABASE::getInstance()
-                ->query('SELECT id, name FROM tl_contaoMaps ORDER BY name')
-                ->fetchAllAssoc();
+try {   // if not installed
+    $rs = DATABASE::getInstance()
+                  ->query('SELECT id, name FROM tl_contaoMaps ORDER BY name')
+                  ->fetchAllAssoc();
 
-foreach ($rs as $map) {
-    $maps[$map['id']] = $map['name'];
-}
+    foreach ($rs as $map) {
+        $maps[$map['id']] = $map['name'];
+    }
 
 
 // Palettes
-$GLOBALS['TL_DCA']['tl_content']['palettes']['map_list'] =
-    '{type_legent},type,headline;'.
-    '{protected_legend:hide},protected;'.
-    '{expert_legend:hide},guest,cssID,space;'.
-    '{invisible_legend:hide},invisible,start,stop';
+    $GLOBALS['TL_DCA']['tl_content']['palettes']['map_list'] =
+        '{type_legent},type,headline;'.
+        '{protected_legend:hide},protected;'.
+        '{expert_legend:hide},guest,cssID,space;'.
+        '{invisible_legend:hide},invisible,start,stop';
 
-$GLOBALS['TL_DCA']['tl_content']['palettes']['single_map'] =
-    '{type_legent},type,headline;'.
-    '{protected_legend:hide},protected;'.
-    '{contaoMaps_legend},cmMapId;'.
-    '{expert_legend:hide},guest,cssID,space;'.
-    '{invisible_legend:hide},invisible,start,stop';
+    $GLOBALS['TL_DCA']['tl_content']['palettes']['single_map'] =
+        '{type_legent},type,headline;'.
+        '{protected_legend:hide},protected;'.
+        '{contaoMaps_legend},cmMapId;'.
+        '{expert_legend:hide},guest,cssID,space;'.
+        '{invisible_legend:hide},invisible,start,stop';
 
 // Fields
-$GLOBALS['TL_DCA']['tl_content']['fields']['cmMapId'] = array
-(
-    'label'     => &$GLOBALS['TL_LANG']['tl_contaoMaps']['ce']['cmMapId'],
-    'exclude'   => true,
-    'search'    => true,
-    'default'   => 0,
-    'inputType' => 'select',
-    'options'   => $maps,
-    'eval'      => array(
-        'mandatory'=> true,
-        'tl_class' => 'clr w50'
-    ),
-    'sql'       => "int NULL"
-);
+    $GLOBALS['TL_DCA']['tl_content']['fields']['cmMapId'] = array
+    (
+        'label'     => &$GLOBALS['TL_LANG']['tl_contaoMaps']['ce']['cmMapId'],
+        'exclude'   => true,
+        'search'    => true,
+        'default'   => 0,
+        'inputType' => 'select',
+        'options'   => $maps,
+        'eval'      => array(
+            'mandatory' => true,
+            'tl_class'  => 'clr w50'
+        ),
+        'sql'       => "int NULL"
+    );
+} catch (Exception $e) {
+    trigger_error('contaoMaps is on disk but not installed', E_USER_NOTICE);
+}
